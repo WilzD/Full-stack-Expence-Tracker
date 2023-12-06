11ce46218afb3de54a2745b4dc0ba9c60e749bb2
WilzD <wilsondynamic3@gmail.com>
2023-12-07 00:21:55 +0530

    user backend (user authentication done)

--- controller/userController.js
+++ controller/userController.js
@@ -21,21 +21,30 @@ exports.postUser = async (req, res) => {
 
 exports.getUser = async (req, res) => {
     try {
-        const name = req.body.name
+        const name=req.body.name
         const email = req.body.email
         const password = req.body.password
+        const data=await User.findAll({where:{email:email}})
 
-        // console.log(email,name,password)
-        const data = await User.findAll();
-        if(data[0].name===name && data[0].email===email && data[0].password===password  ){
-            return res.status(200).json(data)
-        }else{
-            return res.status(403).json(data)
+        if(data.length>0){
+             if(data[0].name===name){
+                if(data[0].password===password){
+                    res.status(200).json({success:true,message:'user login successfully'})
+                }
+                else{
+                    return res.status(402).json({success:false,message:"wrong password"})
+                } 
+             }
+             else{
+               return res.status(403).json({success:false,message:"username not found"})
+             }
+        }
+        else{
+            return res.status(403).json({success:false,message:'wrong email id'})
         }
     }
     catch (error) {
-        let data = 'error'
-        console.log(error)
-        return res.status(404).json(data)
+       return  res.status(404).json({success:false,message:"user not found"})
     }
 }
+
